9a217230dc284deb4af901817448dbd05012211a
Bboiko <b.boyko@mits.pl.com>
2022-04-04 15:34:19 +0200

    side-nav component added

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  form: FormGroup = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required]
+  })
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
   }
 
+  get email() { return this.form.get('email') }
+  get password() { return this.form.get('password') }
+  onSubmit() {
+    console.log(this.form)
+  }
 }
