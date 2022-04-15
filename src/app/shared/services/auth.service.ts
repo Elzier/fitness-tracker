b287c19a5057ca6d6a24a4b1b23115f5bbf67821
Bboiko <b.boyko@mits.pl.com>
2022-04-15 18:18:15 +0200

    auth status listener added

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -4,6 +4,8 @@ import { Router } from '@angular/router'
 import { Injectable } from '@angular/core'
 import { AngularFireAuth } from '@angular/fire/compat/auth'
 import { TrainingService } from './training.service'
+import firebase from 'firebase/compat'
+import User = firebase.User
 
 @Injectable()
 export class AuthService {
@@ -12,21 +14,33 @@ export class AuthService {
 
   constructor(private router: Router, private fbAuth: AngularFireAuth, private trainingService: TrainingService) {}
 
+  initAuthListener() {
+    this.fbAuth.authState.subscribe((user: User | null) => {
+      if (user) {
+        this.isAuthenticated = true
+        this.authChange.next(true)
+        this.router.navigate(['/training'])
+      }
+      else {
+        this.trainingService.cancelAllSubs()
+        this.isAuthenticated = false
+        this.authChange.next(false)
+        this.router.navigate(['/login'])
+      }
+    })
+  }
 
   async login(authData: AuthData) {
     try {
       await this.fbAuth.signInWithEmailAndPassword(authData.email, authData.password)
-      this.authSuccessfully()
     } catch (e) {
       console.error(e)
     }
-    this.authSuccessfully()
   }
 
   async registerUser(authData: AuthData) {
     try {
       await this.fbAuth.createUserWithEmailAndPassword(authData.email, authData.password)
-      this.authSuccessfully()
     } catch (e) {
       console.error(e)
     }
@@ -37,17 +51,7 @@ export class AuthService {
   }
 
   async logout() {
-    this.trainingService.cancelAllSubs()
     await this.fbAuth.signOut()
-    this.isAuthenticated = false
-    this.authChange.next(false)
-    this.router.navigate(['/login'])
-  }
-
-  private authSuccessfully() {
-    this.isAuthenticated = true
-    this.authChange.next(true)
-    this.router.navigate(['/training'])
   }
 
 }
