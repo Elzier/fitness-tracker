b64fc1895ca217976e004462d5144316f7193a15
Bboiko <b.boyko@mits.pl.com>
2022-04-17 12:03:03 +0200

    trainings page canLoad guard added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { AuthGuard } from './shared/services/auth.guard'
 const routes: Routes = [
   {path: '', component: WelcomeComponent},
   {path: 'welcome', component: WelcomeComponent},
-  {path: 'training', loadChildren: () => import('./training/training.module').then(m => m.TrainingModule)}
+  {path: 'training', loadChildren: () => import('./training/training.module').then(m => m.TrainingModule), canLoad: [AuthGuard]}
 ]
 
 @NgModule({
--- src/app/training/training-routing.module.ts
+++ src/app/training/training-routing.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 import { TrainingComponent } from './trainings-page/training.component'
-import { AuthGuard } from '../shared/services/auth.guard'
 
-const routes: Routes = [{path: '', component: TrainingComponent, canActivate: [AuthGuard]}]
+const routes: Routes = [{path: '', component: TrainingComponent}]
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
