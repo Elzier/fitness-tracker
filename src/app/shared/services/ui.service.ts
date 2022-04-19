d7ad7a4423471a7bc809982e1068d7a6b38661cf
Bboiko <b.boyko@mits.pl.com>
2022-04-19 23:04:32 +0200

    mend

--- src/app/shared/services/ui.service.ts
+++ src/app/shared/services/ui.service.ts
@@ -1,23 +1,11 @@
 import { Injectable } from '@angular/core'
-import { Subject } from 'rxjs'
 import { MatSnackBar } from '@angular/material/snack-bar'
 import { MatSnackBarConfig } from '@angular/material/snack-bar/snack-bar-config'
 
 @Injectable({providedIn: 'root'})
 
 export class UIService {
-  loaderStateChanged = new Subject<boolean>()
-
-  constructor(private _snack: MatSnackBar,) {
-  }
-
-  showLoader() {
-    this.loaderStateChanged.next(true)
-  }
-
-  hideLoader() {
-    this.loaderStateChanged.next(false)
-  }
+  constructor(private _snack: MatSnackBar) {}
 
   showSnack(message: string, action: string, config: MatSnackBarConfig) {
     this._snack.open(message, action, config)
