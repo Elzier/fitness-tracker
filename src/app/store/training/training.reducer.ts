0f3e65e44623c54a2a73d20d04ffa31d7c63e370
Bboiko <b.boyko@mits.pl.com>
2022-04-20 15:19:21 +0200

    training store module configured

---
+++ src/app/store/training/training.reducer.ts
@@ -0,0 +1,71 @@
+import {
+  SET_AVAILABLE_EXERCISES,
+  SET_FINISHED_EXERCISES,
+  START_TRAINING,
+  STOP_TRAINING,
+  TrainingAction
+} from './training.actions'
+import { Exercise } from '../../shared/models'
+import * as fromRoot from '../../store/app.reducer'
+import { createFeatureSelector, createSelector } from '@ngrx/store'
+
+export interface TrainingState {
+  availableExercises: Exercise[],
+  finishedExercises: Exercise[],
+  activeTraining: Exercise | null
+}
+
+export interface State extends fromRoot.State {
+  training: TrainingState
+}
+
+const initialState: TrainingState = {
+  availableExercises: [],
+  finishedExercises: [],
+  activeTraining: null
+}
+
+export function trainingReducer(state = initialState, action: TrainingAction) {
+  switch (action.type) {
+    case SET_AVAILABLE_EXERCISES:
+      return {
+        ...state,
+        availableExercises: action.payload
+      }
+    case SET_FINISHED_EXERCISES:
+      return {
+        ...state,
+        finishedExercises: action.payload
+      }
+    case START_TRAINING:
+      return {
+        ...state,
+         activeTraining: action.payload
+      }
+    case STOP_TRAINING:
+      return {
+        ...state,
+        activeTraining: null
+      }
+    default:
+      return state
+  }
+}
+
+
+const getTrainingState = createFeatureSelector<TrainingState>('training')
+
+export const getAvailableExercises = createSelector(
+  getTrainingState,
+  (state: TrainingState) => state.availableExercises
+)
+
+export const getFinishedExercises = createSelector(
+  getTrainingState,
+  (state: TrainingState) => state.finishedExercises
+)
+
+export const getIsExerciseActive = createSelector(
+  getTrainingState,
+  (state: TrainingState) => state.activeTraining != null
+  )
